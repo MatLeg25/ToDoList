e0e6591ca94586036519c7d402fecd8b2817aa5c
MatLeg25 <mat.legierskii@gmail.com>
2021-11-16 22:33:19 +0100

    addNewTask form implemented

--- app/src/main/java/com/example/todolist/AddNewTask.kt
+++ app/src/main/java/com/example/todolist/AddNewTask.kt
@@ -2,10 +2,21 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class AddNewTask : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_new_task)
+
+        // get reference to button
+        val button_submit = findViewById(R.id.button_submit) as Button
+        // set on-click listener
+        button_submit.setOnClickListener {
+            // your code to perform when the user clicks on the button
+            Toast.makeText(this@AddNewTask, "New task added!", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -26,29 +26,9 @@ class MainActivity : AppCompatActivity() {
 //        var array = arrayOf("Task1", "Task2", "Task3", "Task4", "Task5")
 //
 //
-//        // get reference to button
-//        val btn_click_me = findViewById(R.id.btn_add_task) as Button
-//        // set on-click listener
-//        btn_click_me.setOnClickListener {
-//            // your code to perform when the user clicks on the button
-//            Toast.makeText(this@MainActivity, "New task added!", Toast.LENGTH_SHORT).show()
-//        }
-//
-//
-//        val ll_main = findViewById(R.id.ll_main_layout) as LinearLayout
-//
-//        // creating the button
-//        val button_dynamic = Button(this)
-//        // setting layout_width and layout_height using layout parameters
-//        button_dynamic.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
-//        button_dynamic.text = "Dynamic Button"
-//        // add Button to LinearLayout
-//        ll_main.addView(button_dynamic)
+
+
 
     }
 
-    /** Called when the user touches the button */
-    fun sendMessage(view: View) {
-        // Do something in response to button click
-    }
 }
