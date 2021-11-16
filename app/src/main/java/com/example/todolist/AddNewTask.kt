8bff85f4ce1030287600f44944ccf55668c72b84
MatLeg25 <mat.legierskii@gmail.com>
2021-11-17 00:10:37 +0100

    NewTaskAdd - get user input in progres

--- app/src/main/java/com/example/todolist/AddNewTask.kt
+++ app/src/main/java/com/example/todolist/AddNewTask.kt
@@ -2,7 +2,10 @@ package com.example.todolist
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 
 class AddNewTask : AppCompatActivity() {
@@ -10,13 +13,21 @@ class AddNewTask : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_new_task)
 
-        // get reference to button
-        val button_submit = findViewById(R.id.button_submit) as Button
+        val newTask = findViewById<EditText>(R.id.taskDescription)
+        val message = "New task added! " + "(" +newTask.text + ")"
+        val displayMessage = findViewById<TextView>(R.id.message2)
+        var info = "START| "
+        val submitButton = findViewById<Button>(R.id.button_submit)
         // set on-click listener
-        button_submit.setOnClickListener {
+        submitButton.setOnClickListener {
             // your code to perform when the user clicks on the button
-            Toast.makeText(this@AddNewTask, "New task added!", Toast.LENGTH_SHORT).show()
+            displayMessage.text = newTask.text
+            val toast1 = Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT)
+            toast1.setGravity(Gravity.CENTER,0,0)
+            toast1.show()
         }
+
+
 
     }
 }
